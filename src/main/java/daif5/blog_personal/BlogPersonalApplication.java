47f3f34ed0e161deb6c8240e50b3a88bbfcb2b99
nguyenngocdai <ngocdai15101991@gmail.com>
2019-12-12 05:53:32 +0700

    create Jwt Provider

--- src/main/java/daif5/blog_personal/BlogPersonalApplication.java
+++ src/main/java/daif5/blog_personal/BlogPersonalApplication.java
@@ -1,12 +1,19 @@
 package daif5.blog_personal;
 
 import daif5.blog_personal.security.services.UserDetailsServiceImpl;
+import daif5.blog_personal.service.RoleService;
 import daif5.blog_personal.service.UserService;
+import daif5.blog_personal.service.impl.RoleServiceImpl;
 import daif5.blog_personal.service.impl.UserServiceImpl;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BlogPersonalApplication {
@@ -22,5 +29,22 @@ public class BlogPersonalApplication {
     @Bean
     public UserService userService() {
         return new UserServiceImpl();
+    }
+    @Bean
+    public RoleService roleService(){
+        return new RoleServiceImpl();
+    }
+    @Configuration
+    static
+    class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+        private ApplicationContext appContext;
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            appContext = applicationContext;
+        }
+
+
     }
 }
